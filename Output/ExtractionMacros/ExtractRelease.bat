94d4d2de3d973da9b47cf910d270026aa7d4343e
Jean-Loup Kahloun <jean-loup.kahloun@invenietis.com>
2012-01-27 14:52:31 +0000

    - adapting the extractionmacros .bat to the change of folder

--- Output/ExtractionMacros/ExtractRelease.bat
+++ Output/ExtractionMacros/ExtractRelease.bat
@@ -1,16 +1,16 @@
-xcopy ..\CK.Context\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Core\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Interop\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Plugin.Config\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Plugin.Config.Model\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Plugin.Discoverer\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Plugin.Discoverer.Runner\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Plugin.Host\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Plugin.Host.Tests\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Plugin.Model\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Plugin.Runner\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Reflection\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.SharedDic\bin\Release ..\Output\Release\ /y
-xcopy ..\CK.Storage\bin\Release ..\Output\Release\ /y
+xcopy ..\..\CK.Context\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Core\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Interop\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Plugin.Config\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Plugin.Config.Model\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Plugin.Discoverer\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Plugin.Discoverer.Runner\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Plugin.Host\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Plugin.Host.Tests\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Plugin.Model\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Plugin.Runner\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Reflection\bin\Release ..\Release\ /y
+xcopy ..\..\CK.SharedDic\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Storage\bin\Release ..\Release\ /y
 
-del Runtime\Release\*.pdb
+del ..\Release\*.pdb
