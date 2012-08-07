0cfdf9c82a6d92c5e58fe00f84a4a958230ceab0
Maxime Paquatte <maxime.paquatte@gmail.com>
2012-08-07 12:51:09 +0200

    Add ActivityLoggerConsoleColorSink based on ActivityLoggerConsoleSink

---
+++ CK.Core/ActivityLogger/Impl/ActivityLoggerConsoleColorSink.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CK.Core
+{
+    public class ActivityLoggerConsoleColorSink : ActivityLoggerConsoleSink
+    {
+        ConsoleColor _defaultColor;
+        Stack<ConsoleColor> _colors;
+
+
+        public ActivityLoggerConsoleColorSink()
+        {
+            _colors = new Stack<ConsoleColor>();
+            _defaultColor = System.Console.ForegroundColor;
+            _colors.Push(_defaultColor);
+        }
+
+        protected override void OnEnterLevel(LogLevel level, string text)
+        {
+            ConsoleColor c = System.Console.ForegroundColor = GetColor(level);
+            _colors.Push(c);
+            base.OnEnterLevel(level, text);
+        }
+
+        protected override void OnLeaveLevel(LogLevel level)
+        {
+            base.OnLeaveLevel(level);
+            _colors.Pop();
+            System.Console.ForegroundColor = _colors.Peek();
+        }
+
+
+        ConsoleColor GetColor(LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.Error:
+                    return ConsoleColor.Red;
+                case LogLevel.Fatal:
+                    return ConsoleColor.DarkRed;
+                case LogLevel.Info:
+                    return ConsoleColor.Cyan;
+                case LogLevel.None:
+                    return _defaultColor;
+                case LogLevel.Trace:
+                    return ConsoleColor.Gray;
+                case LogLevel.Warn:
+                    return ConsoleColor.Yellow;
+                default:
+                    return _defaultColor;
+            }
+        }
+
+    }
+}
