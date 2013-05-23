599db0a0530cf3f52cdadbc6b451a7048fefac7b
Jean-Loup Kahloun <jean-loup.kahloun@invenietis.com>
2013-05-23 10:18:53 +0200

    Adding the CallerMemberNameAttribute
    This attribute is used to enable the CallerMemberName feature on a .NET 4.0 solution.
     Make sure you are using VS 2012 or higher. If not, CallerMemberName won't inject the name of the caller where it is used.
    
    (Main use at the time of commit : calling WPF OnPropertyChanged with no specified property name)

---
+++ CK.Core/LegacySupport/CallerMemberNameAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Runtime.CompilerServices
+{
+    //This attribute is used to enable the CallerMemberName feature on a .NET 4.0 solution.
+    //Make sure you are using VS 2012 or higher. If not, CallerMemberName won't inject the name of the caller where it is used.
+#if NET40
+    [AttributeUsage( AttributeTargets.Parameter, AllowMultiple = false, Inherited = true )]
+    public sealed class CallerMemberNameAttribute : Attribute
+    {
+    }
+#endif
+}
