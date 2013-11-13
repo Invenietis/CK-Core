b130cd73eef17566685eac41746fd59b581cc6f1
Antoine Blanchet <antoine.blanchet@invenietis.com>
2013-11-13 15:15:52 +0100

    Implement new util for string
    Allow to do string.format with named placeholders like {{placeholderName}}

---
+++ CK.Core/Util.String.cs
@@ -0,0 +1,98 @@
+#region LGPL License
+/*----------------------------------------------------------------------------
+* This file (CK.Core\Util.String.cs) is part of CiviKey. 
+*  
+* CiviKey is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Lesser General Public License as published 
+* by the Free Software Foundation, either version 3 of the License, or 
+* (at your option) any later version. 
+*  
+* CiviKey is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+* GNU Lesser General Public License for more details. 
+* You should have received a copy of the GNU Lesser General Public License 
+* along with CiviKey.  If not, see <http://www.gnu.org/licenses/>. 
+*  
+* Copyright © 2007-2012, 
+*     Invenietis <http://www.invenietis.com>,
+*     In’Tech INFO <http://www.intechinfo.fr>,
+* All rights reserved. 
+*-----------------------------------------------------------------------------*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Dynamic;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace CK.Core
+{
+    /// <summary>
+    /// Utility class.
+    /// </summary>
+    static public partial class Util
+    {
+        /// <summary>
+        /// Offers extensibility methods for strings.
+        /// </summary>
+        static public class String
+        {
+            const string OpenTag = "{{";
+            const string CloseTag = "}}";
+
+            static Regex _namedFormatRegex = new Regex( string.Format( "{0}(?<token>.*?){1}", OpenTag, CloseTag ) );
+            static Regex _validPlaceHolderName = new Regex( "^([a-zA-Z0-9])+$" );
+
+            /// <summary>
+            /// String.Format that works with named place holders like {{hello}} instead of {0}
+            /// </summary>
+            /// <param name="format">The format string</param>
+            /// <param name="values">An object that will give named parameters values</param>
+            /// <returns>Formatted string</returns>
+            public static string NamedFormat( string format, dynamic values )
+            {
+                if( string.IsNullOrEmpty( format ) )
+                    throw new ArgumentNullException( "format" );
+                if( values == null )
+                    throw new ArgumentNullException( "values" );
+
+                int lastIdx = 0;
+                StringBuilder result = new StringBuilder();
+                var match = _namedFormatRegex.Match( format );
+                while( match.Success )
+                {
+                    Group token = match.Groups["token"];
+                    if( !_validPlaceHolderName.IsMatch( token.Value ) )
+                        throw new ArgumentException( string.Format( "The value '{0}' is not a valid named placeholder", token.Value ) );
+
+                    int tokenIdx = token.Index - OpenTag.Length;
+                    int tokenLength = token.Length + (OpenTag.Length + CloseTag.Length);
+
+                    result.Append( format.Substring( lastIdx, tokenIdx - lastIdx ) );
+
+                    try
+                    {
+                        result.Append( values.GetType().GetProperty( token.Value ).GetValue( values, null ) );
+                    }
+                    catch( RuntimeBinderException ex )
+                    {
+                        throw new ArgumentException( string.Format( "Unable to find the property '{0}' on the given value object", token.Value ), ex );
+                    }
+
+                    lastIdx = tokenIdx + tokenLength;
+
+                    match = match.NextMatch();
+                }
+
+                if( lastIdx < format.Length )
+                    result.Append( format.Substring( lastIdx ) );
+
+                return result.ToString();
+            }
+        }
+    }
+}
---
+++ Tests/CK.Core.Tests/StringUtilTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CK.Core.Tests
+{
+    [TestFixture]
+    [Category("String.Util")]
+    public class StringUtilTests
+    {
+        [Test]
+        public void TestNamedFormat()
+        {
+            string format = "Hi {{username}}, my name is {{me}}. How are you ? If you want to contact me just send me an email to {{me}}@gmail.com. Bye !";
+            string formatted = Util.String.NamedFormat( format, new { username = "somerandomuser", me = "thesendername" } );
+
+            Assert.That( formatted, Is.EqualTo( "Hi somerandomuser, my name is thesendername. How are you ? If you want to contact me just send me an email to thesendername@gmail.com. Bye !" ) );
+        }
+
+        [Test]
+        public void TestWrongNamedFormat()
+        {
+            var o = new { };
+
+            Assert.Throws<ArgumentException>( () =>
+            {
+                Util.String.NamedFormat( "Hi {{0wrongname}}", o );
+            } );
+            Assert.Throws<ArgumentException>( () =>
+            {
+                Util.String.NamedFormat( "Hi {{wrong name}}", o );
+            } );
+            Assert.Throws<ArgumentException>( () =>
+            {
+                Util.String.NamedFormat( "Hi {{wrong-name}}", o );
+            } );
+            Assert.Throws<ArgumentException>( () =>
+            {
+                Util.String.NamedFormat( "Hi {{}}", o );
+            } );
+        }
+
+        [Test]
+        public void TestNullValueObject()
+        {
+            Assert.Throws<ArgumentNullException>( () =>
+            {
+                Util.String.NamedFormat( "Hi {{name}}", null );
+            } );
+        }
+
+        [Test]
+        public void TestMissingProperty()
+        {
+            Assert.Throws<ArgumentException>( () =>
+            {
+                Util.String.NamedFormat( "Hi {{name}}", new { propName = "toto" } );
+            } );
+        }
+    }
+}
