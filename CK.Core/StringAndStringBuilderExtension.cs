9739cce56b2fad68ce4a5f9fdfbd5f91c8d9070c
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-04-11 10:24:26 +0200

    Changed the name of Append extension method.

--- CK.Core/StringAndStringBuilderExtension.cs
+++ CK.Core/StringAndStringBuilderExtension.cs
@@ -16,17 +16,18 @@ namespace CK.Core
         /// <returns>The joined string.</returns>
         public static string Concatenate( this IEnumerable<string> @this, string separator = ", " )
         {
-            return new StringBuilder().Append( @this, separator ).ToString();
+            return new StringBuilder().AppendStrings( @this, separator ).ToString();
         }
 
         /// <summary>
         /// Appends a set of strings with an internal separator.
+        /// (This should be named 'Append' but appropriate overload is not always detected by the compiler.)
         /// </summary>
         /// <param name="this">The <see cref="StringBuilder"/> to append to.</param>
         /// <param name="strings">Set of strings. Can be null.</param>
         /// <param name="separator">The separator string.</param>
         /// <returns>The builder itself.</returns>
-        public static StringBuilder Append( this StringBuilder @this, IEnumerable<string> strings, string separator = ", " )
+        public static StringBuilder AppendStrings( this StringBuilder @this, IEnumerable<string> strings, string separator = ", " )
         {
             if( strings != null )
             {
