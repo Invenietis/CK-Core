5501e6274011a97176daefeaa9b8d833025b03b9
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-11-03 18:27:53 +0100

    Added CodeCakeBuilder DNX project.

---
+++ CodeCakeBuilder/CommandRunner.cs
@@ -0,0 +1,66 @@
+﻿using Cake.Core;
+using Cake.Core.Diagnostics;
+using Cake.Common.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+
+namespace Code.Cake
+{
+    static class CommandRunner
+    {
+        public static int RunCmd( this ICakeContext context, string commandLine )
+        {
+            ProcessStartInfo cmdStartInfo = new ProcessStartInfo();
+            cmdStartInfo.FileName = @"cmd.exe";
+            cmdStartInfo.RedirectStandardOutput = true;
+            cmdStartInfo.RedirectStandardError = true;
+            cmdStartInfo.RedirectStandardInput = true;
+            cmdStartInfo.UseShellExecute = false;
+            cmdStartInfo.CreateNoWindow = true;
+
+            Process cmdProcess = new Process();
+            cmdProcess.StartInfo = cmdStartInfo;
+            cmdProcess.ErrorDataReceived += ( o, e ) => { if( !string.IsNullOrEmpty( e.Data ) ) context.Error( e.Data ); };
+            cmdProcess.OutputDataReceived += ( o, e ) => { if( !string.IsNullOrEmpty( e.Data ) ) context.Information( e.Data ); };
+            cmdProcess.EnableRaisingEvents = true;
+            cmdProcess.Start();
+            cmdProcess.BeginOutputReadLine();
+            cmdProcess.BeginErrorReadLine();
+
+            cmdProcess.StandardInput.WriteLine( commandLine + " & exit" );
+            cmdProcess.WaitForExit();
+            return cmdProcess.ExitCode;
+        }
+
+        /// <summary>
+        /// Runs dnu restore.
+        /// </summary>
+        /// <param name="context">This cake context.</param>
+        /// <param name="config">The configuration to use.</param>
+        public static void DNURestore( this ICakeContext context, Action<DNURestoreSettings> config )
+        {
+            var c = new DNURestoreSettings();
+            config( c );
+            var b = new StringBuilder();
+            b.Append( "dnu " );
+            c.ToString( b );
+            RunCmd( context, b.ToString() );
+        }
+
+        public static void DNUBuild( this ICakeContext context, Action<DNUBuildSettings> config )
+        {
+            var c = new DNUBuildSettings();
+            config( c );
+            var b = new StringBuilder();
+            b.Append( "dnu " );
+            c.ToString( b );
+            RunCmd( context, b.ToString() );
+        }
+
+    }
+}
---
+++ CodeCakeBuilder/Program.cs
@@ -0,0 +1,35 @@
+﻿using Code.Cake;
+using CodeCake;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Dnx.Runtime;
+using System.IO;
+
+namespace CodeCakeBuilder
+{
+    public class Program
+    {
+        string _solutionDir;
+
+        public Program( IApplicationEnvironment appEnv )
+        {
+            _solutionDir = Path.GetDirectoryName( appEnv.ApplicationBasePath );
+        }
+
+        public int Main( string[] args )
+        {
+            var app = new CodeCakeApplication( _solutionDir, typeof(Program).Assembly );
+            bool interactive = !args.Contains( '-' + InteractiveAliases.NoInteractionArgument, StringComparer.OrdinalIgnoreCase );
+            int result = app.Run( args );
+            Console.WriteLine();
+            if( interactive )
+            {
+                Console.WriteLine( "Hit any key to exit. (Use -{0} parameter to exit immediately)", InteractiveAliases.NoInteractionArgument );
+                Console.ReadKey();
+            }
+            return result;
+        }
+    }
+}
