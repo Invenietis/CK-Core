73f423a5ee4136d1924808bea05d69a8c5e604be
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-11-10 15:00:31 +0100

    Attempt to use NUnitLite (AutoRun for Main entry point).

--- CodeCakeBuilder/Build.cs
+++ CodeCakeBuilder/Build.cs
@@ -66,7 +66,7 @@ namespace CodeCake
             Task( "Set-ProjectVersion" )
                 .Does( () =>
                 {
-                    if( dnxSolution.UpdateProjectFiles() > 0 )
+                    if( dnxSolution.UpdateProjectFiles( true ) > 0 )
                     {
                         Cake.DNURestore( c =>
                         {
---
+++ Tests/CK.Core.Tests/Program.cs
@@ -0,0 +1,26 @@
+﻿using NUnitLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CK.Core.Tests
+{
+    public class Program
+    {
+        public int Main( string[] arguments )
+        {
+            int result;
+#if DNX451
+            result = new AutoRun().Execute( arguments );
+#else
+            var sw = new StringWriter();
+            var sr = new StringReader("");
+            result = new AutoRun().Execute( typeof(Program).GetTypeInfo().Assembly, sw, sr, arguments );
+#endif
+            return result;
+        }
+    }
+}
