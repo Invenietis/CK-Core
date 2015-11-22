2eeddd396c2028cd8e7156d1a477c59600314ac8
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-11-22 13:25:54 +0100

    Not using the Bootstrap.cmd

--- CodeCakeBuilder/Bootstrap.cmd
+++ CodeCakeBuilder/Bootstrap.cmd
@@ -1,4 +1,6 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
-dnvm list
+dnvm use -p -r clr 1.0.0-rc1-final
 dnu feeds list
+pause
 dnu restore
+pause
