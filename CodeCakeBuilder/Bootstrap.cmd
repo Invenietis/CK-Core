39ccac4816822d04f702f85b88932584ae001fc0
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-11-22 13:29:15 +0100

    Another try...

--- CodeCakeBuilder/Bootstrap.cmd
+++ CodeCakeBuilder/Bootstrap.cmd
@@ -1,6 +1,1 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
-dnvm use -p -r clr 1.0.0-rc1-final
-dnu feeds list
-pause
-dnu restore
-pause
