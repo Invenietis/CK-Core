211314d01c0944b330ed15871e530b390a0cf393
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-11-22 12:35:46 +0100

    Added dnu restore.

--- CodeCakeBuilder/Bootstrap.cmd
+++ CodeCakeBuilder/Bootstrap.cmd
@@ -1,2 +1,3 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 dnvm list
+dnu restore
