9ff770f9254687763dd7453cdc616d562dd5ef68
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-11-22 12:40:15 +0100

    Need explicit feeds?

--- CodeCakeBuilder/Bootstrap.cmd
+++ CodeCakeBuilder/Bootstrap.cmd
@@ -1,3 +1,4 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 dnvm list
+dnu feeds list
 dnu restore
