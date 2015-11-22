ff8730fb8cbe0e7eed47574eae321397cd04f627
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-11-22 12:02:13 +0100

    Try...

--- CodeCakeBuilder/Bootstrap.cmd
+++ CodeCakeBuilder/Bootstrap.cmd
@@ -1,5 +1,5 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
 
-dnvm install 1.0.0-rc1-final
+rem dnvm install 1.0.0-rc1-final
 
 dnvm list
