fc83c85ef79d6aefe016a9b6c9f8e0c4db8d826e
Cédric Legendre <albundy1385@gmail.com>
2016-03-21 10:42:18 +0100

    Restore CK.Reflection

---
+++ CK.Reflection/EmitHelper.cs
@@ -0,0 +1,161 @@
+#region LGPL License
+/*----------------------------------------------------------------------------
+* This file (CK.Reflection\EmitHelper.cs) is part of CiviKey. 
+*  
+* CiviKey is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Lesser General Public License as published 
+* by the Free Software Foundation, either version 3 of the License, or 
+* (at your option) any later version. 
+*  
+* CiviKey is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+* GNU Lesser General Public License for more details. 
+* You should have received a copy of the GNU Lesser General Public License 
+* along with CiviKey.  If not, see <http://www.gnu.org/licenses/>. 
+*  
+* Copyright © 2007-2015, 
+*     Invenietis <http://www.invenietis.com>,
+*     In’Tech INFO <http://www.intechinfo.fr>,
+* All rights reserved. 
+*-----------------------------------------------------------------------------*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Diagnostics;
+
+namespace CK.Reflection
+{
+    /// <summary>
+    /// Collection of helpers to emit dynamic code. 
+    /// </summary>
+    public static class EmitHelper
+    {
+        /// <summary>
+        /// Implements a method as a no operation method. Method can be virtual, abstract or not.
+        /// </summary>
+        /// <param name="tB">The <see cref="TypeBuilder"/> for the new type.</param>
+        /// <param name="method">The method to implement.</param>
+        /// <param name="isVirtual">Defaults to false: the method is sealed. True to keep the method virtual. </param>
+        /// <returns>The <see cref="MethodBuilder"/> to enable, for instance, creation of custom attributes on the method.</returns>
+        public static MethodBuilder ImplementEmptyStubMethod( TypeBuilder tB, MethodInfo method, bool isVirtual = false )
+        {
+            if( tB == null ) throw new ArgumentNullException( "tB" );
+            if( method == null ) throw new ArgumentNullException( "method" );
+
+            ParameterInfo[] parameters = method.GetParameters();
+            Type[] parametersTypes = ReflectionHelper.CreateParametersType( parameters );
+            Type returnType = method.ReturnType;
+
+            MethodAttributes mA = method.Attributes & ~(MethodAttributes.Abstract | MethodAttributes.VtableLayoutMask);
+            if( isVirtual ) mA |= MethodAttributes.Virtual;
+            MethodBuilder mB = tB.DefineMethod( method.Name, mA );
+            if( method.ContainsGenericParameters )
+            {
+                int i = 0;
+
+                Type[] genericArguments = method.GetGenericArguments();
+                string[] names = genericArguments.Select( t => String.Format( "T{0}", i++ ) ).ToArray();
+
+                var genericParameters = mB.DefineGenericParameters( names );
+                for( i = 0; i < names.Length; ++i )
+                {
+                    Type genericTypeArgument = genericArguments[i];
+                    GenericTypeParameterBuilder genericTypeBuilder = genericParameters[i];
+
+
+                    genericTypeBuilder.SetGenericParameterAttributes( genericTypeArgument.GenericParameterAttributes );
+                    genericTypeBuilder.SetInterfaceConstraints( genericTypeArgument.GetGenericParameterConstraints() );
+                }
+            }
+            mB.SetReturnType( method.ReturnType );
+            mB.SetParameters( ReflectionHelper.CreateParametersType( parameters ) );
+            EmitEmptyImplementation( mB, returnType, parameters );
+            return mB;
+        }
+
+        private static void EmitEmptyImplementation( MethodBuilder vM, Type returnType, ParameterInfo[] parameters )
+        {
+            ILGenerator gVM = vM.GetILGenerator();
+            for( int i = 0; i < parameters.Length; ++i )
+            {
+                // DefineParameter use 0 for the return parameter.
+                ParameterInfo param = parameters[i];
+                vM.DefineParameter( i + 1, param.Attributes, param.Name );
+                if( param.IsOut )
+                {
+                    Debug.Assert( param.ParameterType.IsByRef, "'Out' is just an attribute on 'by ref' parameters (unfortunate for covariance support)." );
+                    gVM.StoreDefaultValueForOutParameter( param );
+                }
+            }
+            if( returnType != typeof( void ) )
+            {
+                if( returnType.IsValueType )
+                {
+                    LocalBuilder retValue = gVM.DeclareLocal( returnType );
+                    gVM.Emit( OpCodes.Ldloca_S, retValue.LocalIndex );
+                    gVM.Emit( OpCodes.Initobj, returnType );
+                    gVM.LdLoc( retValue );
+                }
+                else
+                {
+                    gVM.Emit( OpCodes.Ldnull );
+                }
+            }
+            gVM.Emit( OpCodes.Ret );
+        }
+
+        /// <summary>
+        /// Implement a property with getter/setter that relies on a private backup field.
+        /// This is useful only to provide a temporary implementation of abstract properties that would be generated in a second time (this does not 
+        /// provide more than auto implemented properties available in C# 3.0 and later.
+        /// </summary>
+        /// <param name="tB">The <see cref="TypeBuilder"/> for the new type.</param>
+        /// <param name="property">The property to implement.</param>
+        /// <param name="isVirtual">Defaults to false: the method is sealed. True to keep the method virtual. </param>
+        /// <returns>The <see cref="PropertyBuilder"/> to enable, for instance, creation of custom attributes on the property.</returns>
+        public static PropertyBuilder ImplementStubProperty( TypeBuilder tB, PropertyInfo property, bool isVirtual = false )
+        {
+            if( tB == null ) throw new ArgumentNullException( "tB" );
+            if( property == null ) throw new ArgumentNullException( "property" );
+
+            FieldBuilder backField = tB.DefineField( "_" + property.Name + property.GetHashCode(), property.PropertyType, FieldAttributes.Private );
+
+            MethodInfo getMethod = property.GetGetMethod( true );
+            MethodBuilder mGet = null;
+            if( getMethod != null )
+            {
+                MethodAttributes mA = getMethod.Attributes & ~(MethodAttributes.Abstract | MethodAttributes.VtableLayoutMask);
+                if( isVirtual ) mA |= MethodAttributes.Virtual;
+                mGet = tB.DefineMethod( getMethod.Name, mA, property.PropertyType, Type.EmptyTypes );
+                ILGenerator g = mGet.GetILGenerator();
+                g.LdArg( 0 );
+                g.Emit( OpCodes.Ldfld, backField );
+                g.Emit( OpCodes.Ret );
+            }
+            MethodInfo setMethod = property.GetSetMethod( true );
+            MethodBuilder mSet = null;
+            if( setMethod != null )
+            {
+                MethodAttributes mA = setMethod.Attributes & ~(MethodAttributes.Abstract | MethodAttributes.VtableLayoutMask);
+                if( isVirtual ) mA |= MethodAttributes.Virtual;
+                mSet = tB.DefineMethod( setMethod.Name, mA, typeof( void ), new[] { property.PropertyType } );
+                ILGenerator g = mSet.GetILGenerator();
+                g.LdArg( 0 );
+                g.LdArg( 1 );
+                g.Emit( OpCodes.Stfld, backField );
+                g.Emit( OpCodes.Ret );
+            }
+
+            PropertyBuilder p = tB.DefineProperty( property.Name, property.Attributes, property.PropertyType, Type.EmptyTypes );
+            if( mGet != null ) p.SetGetMethod( mGet );
+            if( mSet != null ) p.SetSetMethod( mSet );
+            return p;
+        }
+    }
+}
---
+++ CK.Reflection/TypeBuilderExtension.cs
@@ -0,0 +1,107 @@
+#region LGPL License
+/*----------------------------------------------------------------------------
+* This file (CK.Reflection\TypeBuilderExtension.cs) is part of CiviKey. 
+*  
+* CiviKey is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Lesser General Public License as published 
+* by the Free Software Foundation, either version 3 of the License, or 
+* (at your option) any later version. 
+*  
+* CiviKey is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+* GNU Lesser General Public License for more details. 
+* You should have received a copy of the GNU Lesser General Public License 
+* along with CiviKey.  If not, see <http://www.gnu.org/licenses/>. 
+*  
+* Copyright © 2007-2015, 
+*     Invenietis <http://www.invenietis.com>,
+*     In’Tech INFO <http://www.intechinfo.fr>,
+* All rights reserved. 
+*-----------------------------------------------------------------------------*/
+#endregion
+
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace CK.Reflection
+{
+    /// <summary>
+    /// Provides extension methods on <see cref="TypeBuilder"/> class.
+    /// </summary>
+    public static class TypeBuilderExtension
+    {
+        /// <summary>
+        /// Creates constructors that relay calls to public and protected constructors in the base class.
+        /// </summary> 
+        /// <param name="this">This <see cref="TypeBuilder"/>.</param>
+        /// <param name="baseConstructorfilter">
+        /// Optional predicate used to filter constructors that must be implemented and set its <see cref="MethodAttributes"/>. 
+        /// When this function returns null, the constructor is not implemented, otherwise it can return the baseConstructor's Attribute.
+        /// When null, all public and protected constructors are replicated with the same access (public or protected).
+        /// </param>
+        /// <param name="constructorAttributesFilter">
+        /// Optional predicate used to filter constructors' attributes. 
+        /// When null, all attributes are redefined.
+        /// </param>
+        /// <param name="parameterAttributesFilter">
+        /// Optional predicate used to filter constructors' arguments' attributes. 
+        /// When null, all attributes are redefined.
+        /// </param>
+        public static void DefinePassThroughConstructors( this TypeBuilder @this,
+                                                            Func<ConstructorInfo,MethodAttributes?> baseConstructorfilter = null,
+                                                            Func<ConstructorInfo, CustomAttributeData, bool> constructorAttributesFilter = null,
+                                                            Func<ParameterInfo, CustomAttributeData, bool> parameterAttributesFilter = null )
+        {
+            Type baseType = @this.BaseType;
+            foreach( var baseCtor in baseType.GetConstructors( BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic ) )
+            {
+                if( baseCtor.IsPrivate ) continue;
+                MethodAttributes? newCtorAttr = baseCtor.Attributes;
+                if( baseConstructorfilter != null && !(newCtorAttr = baseConstructorfilter( baseCtor )).HasValue ) continue;
+                var parameters = baseCtor.GetParameters();
+                if( parameters.Length == 0 ) @this.DefineDefaultConstructor( newCtorAttr.Value );
+                else
+                {
+                    Type[] parameterTypes = ReflectionHelper.CreateParametersType( parameters );
+                    Type[][] requiredCustomModifiers = parameters.Select( p => p.GetRequiredCustomModifiers() ).ToArray();
+                    Type[][] optionalCustomModifiers = parameters.Select( p => p.GetOptionalCustomModifiers() ).ToArray();
+
+                    var ctor = @this.DefineConstructor( newCtorAttr.Value, baseCtor.CallingConvention, parameterTypes, requiredCustomModifiers, optionalCustomModifiers );
+                    for( var i = 0; i < parameters.Length; ++i )
+                    {
+                        ParameterInfo parameter = parameters[i];
+                        ParameterBuilder pBuilder = ctor.DefineParameter( i + 1, parameter.Attributes, parameter.Name );
+                        if( (parameter.Attributes & ParameterAttributes.HasDefault) != 0 )
+                        {
+                            pBuilder.SetConstant( parameter.RawDefaultValue );
+                        }
+                        if( parameterAttributesFilter != null )
+                        {
+                            ReflectionHelper.GenerateCustomAttributeBuilder( parameter.GetCustomAttributesData(), pBuilder.SetCustomAttribute, a => parameterAttributesFilter( parameter, a ) );
+                        }
+                        else
+                        {
+                            ReflectionHelper.GenerateCustomAttributeBuilder( parameter.GetCustomAttributesData(), pBuilder.SetCustomAttribute );
+                        }
+                    }
+                    if( constructorAttributesFilter != null )
+                    {
+                        ReflectionHelper.GenerateCustomAttributeBuilder( baseCtor.GetCustomAttributesData(), ctor.SetCustomAttribute, a => constructorAttributesFilter( baseCtor, a ) );
+                    }
+                    else
+                    {
+                        ReflectionHelper.GenerateCustomAttributeBuilder( baseCtor.GetCustomAttributesData(), ctor.SetCustomAttribute );
+                    }
+                    var g = ctor.GetILGenerator();
+                    g.RepushActualParameters( true, parameters.Length + 1 );
+                    g.Emit( OpCodes.Call, baseCtor );
+                    g.Emit( OpCodes.Ret );
+                }
+            }
+        }
+
+    }
+}
