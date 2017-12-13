0a4a4d6de3f75638c16857b4c65b61d67499cc19
Olivier Spinelli <olivier.spinelli@invenietis.com>
2017-12-13 17:21:58 +0100

    New WeakAssemblyNameResolver.
    Using invenietis-release feed.

--- CodeCakeBuilder/Build.cs
+++ CodeCakeBuilder/Build.cs
@@ -182,7 +182,7 @@ namespace CodeCake
                             || gitInfo.PreReleaseName == "prerelease"
                             || gitInfo.PreReleaseName == "rc" )
                         {
-                            PushNuGetPackages( "NUGET_API_KEY", "https://www.nuget.org/api/v2/package", nugetPackages );
+                            PushNuGetPackages( "MYGET_RELEASE_API_KEY", "https://www.myget.org/F/invenietis-preview/api/v2/package", nugetPackages );
                         }
                         else
                         {
