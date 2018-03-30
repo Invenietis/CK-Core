fbdefac3f46ad8d09d7f58d71ae1f30fb7814107
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-03-30 10:27:36 +0200

    Global Full Build using LocalFeed.

--- CodeCakeBuilder/Program.cs
+++ CodeCakeBuilder/Program.cs
@@ -1,9 +1,6 @@
-using Code.Cake;
-using CodeCake;
 using System;
-using System.Linq;
 
-namespace CodeCakeBuilder
+namespace CodeCake
 {
     class Program
     {
@@ -11,20 +8,20 @@ namespace CodeCakeBuilder
         /// CodeCakeBuilder entry point. This is a default, simple, implementation that can 
         /// be extended as needed.
         /// </summary>
-        /// <param name="args"></param>
-        /// <returns>An error code (typically -1), 0 on success.</returns>
+        /// <param name="args">The command line arguments.</param>
+        /// <returns>An error code (typically negative), 0 on success.</returns>
         static int Main( string[] args )
         {
             var app = new CodeCakeApplication();
-            bool interactive = !args.Contains( '-' + InteractiveAliases.NoInteractionArgument, StringComparer.OrdinalIgnoreCase );
-            int result = app.Run( args );
-            Console.WriteLine();
-            if( interactive )
+            RunResult result = app.Run( args );
+            if( result.InteractiveMode == InteractiveMode.Interactive )
             {
-                Console.WriteLine( "Hit any key to exit. (Use -{0} parameter to exit immediately)", InteractiveAliases.NoInteractionArgument );
+                Console.WriteLine();
+                Console.WriteLine( $"Hit any key to exit." );
+                Console.WriteLine( $"Use -{InteractiveAliases.NoInteractionArgument} or -{InteractiveAliases.AutoInteractionArgument} parameter to exit immediately." );
                 Console.ReadKey();
             }
-            return result;
+            return result.ReturnCode;
         }
     }
 }
