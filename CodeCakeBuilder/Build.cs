4ed3b5ff81ac4a6998330196ab1a46725dbe4536
Olivier Spinelli <olivier.spinelli@signature-code.com>
2022-01-22 17:13:31 +0100

    Applied settings.

--- CodeCakeBuilder/Build.cs
+++ CodeCakeBuilder/Build.cs
@@ -13,7 +13,6 @@ using Cake.Common.Tools.NUnit;
 using Cake.Core;
 using Cake.Core.Diagnostics;
 using Cake.Core.IO;
-using CK.Text;
 using SimpleGitVersion;
 using System;
 using System.Collections.Generic;
