2afe32a3de510e9ddb0e74c425a0dce309cf0346
Olivier Spinelli <olivier.spinelli@signature-code.com>
2022-11-16 22:58:33 +0100

    Minor NRT fix.

--- Tests/CK.Core.Tests/FIFOTests.cs
+++ Tests/CK.Core.Tests/FIFOTests.cs
@@ -21,7 +21,7 @@ namespace CK.Core.Tests
             for( int iTry = 0; iTry <= f.Capacity; ++iTry )
             {
                 f.Clear();
-                for( int i = 0; i < iTry; ++i ) f.Push( default );
+                for( int i = 0; i < iTry; ++i ) f.Push( default! );
                 foreach( var i in saved ) f.Push( i );
                 while( f.Count > saved.Length ) f.Pop();
                 f.SequenceEqual( saved ).Should().BeTrue();
