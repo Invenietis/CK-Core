4edd2cec7560792419be1e452cee9c99635cd653
Olivier Spinelli <olivier.spinelli@signature-code.com>
2023-09-13 20:21:04 +0200

    Changed JsonReader delegate.

--- CK.Core/Json/Utf8JsonReaderDelegate.cs
+++ CK.Core/Json/Utf8JsonReaderDelegate.cs
@@ -10,17 +10,19 @@ namespace CK.Core.Json
     /// <summary>
     /// Delegate for typed read from a <see cref="Utf8JsonReader"/>.
     /// </summary>
-    /// <param name="r">The reader.</param>
-    /// <param name="context">The context.</param>
-    /// <returns>The read instance or null.</returns>
-    public delegate void Utf8JsonReaderDelegate( ref Utf8JsonReader r, IUtf8JsonReaderContext context );
-
-    /// <summary>
-    /// Delegate for typed read from a <see cref="Utf8JsonReader"/>.
-    /// </summary>
     /// <typeparam name="T">The type to read.</typeparam>
     /// <param name="r">The reader.</param>
     /// <param name="context">The context.</param>
     /// <returns>The read instance or null.</returns>
     public delegate T? Utf8JsonReaderDelegate<T>( ref Utf8JsonReader r, IUtf8JsonReaderContext context );
+
+    /// <summary>
+    /// Delegate for typed read from a <see cref="Utf8JsonReader"/>.
+    /// </summary>
+    /// <typeparam name="T">The type to read.</typeparam>
+    /// <typeparam name="T">The type of the reader context.</typeparam>
+    /// <param name="r">The reader.</param>
+    /// <param name="context">The context.</param>
+    /// <returns>The read instance or null.</returns>
+    public delegate T? Utf8JsonReaderDelegate<T,TReadContext>( ref Utf8JsonReader r, TReadContext context ) where TReadContext : IUtf8JsonReaderContext;
 }
